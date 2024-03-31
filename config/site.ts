29afcccf4dd28c1f1048f49b07420b9eaead08b8
Jaeha Lee <wogkdkrm112@gmail.com>
2024-03-31 22:38:31 +0900

    re-init: tech-blog

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'Jaeha',
+  url: 'https://example.com',
+  description: 'Nextjs 14 blog using velite, tailwind and shadcn',
+  author: 'Jaeha',
+  links: {
+    twitter: 'https://twitter.com/miniapp223',
+    github: 'https://github.com/jaehafe',
+    personalSite: 'https://github.com/jaehafe',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
